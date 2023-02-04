693c1443b8719690275afb0a7bf74343c4a0ea86
Hankrecords <>
2023-02-04 16:50:31 +0100

    - smoother movement of player horde minions

---
+++ Assets/Scripts/HordeWaypoint.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class HordeWaypoint : MonoBehaviour
+{
+    private CharacterController controller;
+    [SerializeField]
+    private float moveSpeed = 0.5f;
+    private Vector2 moveInput = Vector2.zero;
+    public GameObject player;
+    public float maxDistance = 0.3f;
+    private Vector3 currentMoveVelocity;
+    private Vector3 moveDampVelocity;
+
+    private void Awake()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    private void Start() {
+        transform.position = player.transform.position;
+    }
+
+    private void Update()
+    {
+        Vector3 playerInput = new Vector3
+        {
+            x = Input.GetAxisRaw("Horizontal"),
+            y = 0f,
+            z = Input.GetAxisRaw("Vertical"),
+        };
+
+        if (playerInput.magnitude > 1f)
+        {
+            playerInput.Normalize();
+        }
+
+        Vector3 moveVector = transform.TransformDirection(playerInput);
+
+        if (moveVector.Equals(Vector3.zero))
+        {
+            var step =  moveSpeed * Time.deltaTime;
+            transform.localPosition = Vector3.MoveTowards(transform.localPosition, Vector3.zero, step);
+            return;
+        }
+
+        var velocity = moveVector * moveSpeed * Time.deltaTime;
+        Vector3 moveDist = (transform.localPosition + velocity);
+        if (moveDist.magnitude > maxDistance)  
+        {
+            moveDist += velocity * 3;
+            moveDist = moveDist.normalized * maxDistance;
+        }
+        
+        transform.localPosition = moveDist;
+    }
+}
--- Assets/Scripts/MinionNavigation.cs
+++ Assets/Scripts/MinionNavigation.cs
@@ -8,11 +8,13 @@ public class MinionNavigation : MonoBehaviour
     private NavMeshAgent _navMeshAgent;
     [field: SerializeField] public Transform Followtransform { get; set; }
     [field: SerializeField] public float MinDistance { get; set; } = 5f;
-    [field: SerializeField] public string PlayerTag { get; set; } = "Player";
+    [field: SerializeField] public string WaypointTag { get; set; } = "playerHordeWaypoint";
+    [field: SerializeField] public float BaseSpeed {get; set; } = 10f;
+    [field: SerializeField] public float BaseAcceleration {get; set; } = 10f;
 
     void Start()
     {
-        var playerGo = GameObject.FindGameObjectWithTag(PlayerTag);
+        var playerGo = GameObject.FindGameObjectWithTag(WaypointTag);
         Followtransform = playerGo.GetComponent<Transform>();
         _navMeshAgent = GetComponent<NavMeshAgent>();
     }
@@ -24,5 +26,7 @@ public class MinionNavigation : MonoBehaviour
         var destination = Followtransform.position;
         _navMeshAgent.isStopped = Vector3.Distance(transform.position, destination) <= MinDistance;
         _navMeshAgent.destination = destination;
+        _navMeshAgent.speed = (BaseSpeed / (2 / Vector3.Distance(transform.position, destination)));
+        _navMeshAgent.acceleration = (BaseAcceleration / (1.5f / Vector3.Distance(transform.position, destination)));
     }
 }
