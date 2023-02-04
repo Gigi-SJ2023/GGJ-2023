97dff1741903cc2ce33f20f0c7bca76f7523298f
Silvio Pilato <pilatosil@gmail.com>
2023-02-04 16:09:43 +0100

    Add wallcheck to attacks

--- Assets/Scripts/HordeAttack.cs
+++ Assets/Scripts/HordeAttack.cs
@@ -1,33 +1,48 @@
-﻿using System;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.Serialization;
+﻿using UnityEngine;
 
 namespace PlayerHorde
 {
     public class HordeAttack: MonoBehaviour
     {
-        private Timer _timer;
         [field: SerializeField] 
         private HordeMemberType HordeType { get; set; } = HordeMemberType.Carrot;
         [field: SerializeField] 
         public float TickDuration { get; set; } = 5;
         public int DamagePerUnit { get; set; } = 10;
         public int Unit { get; set; } = 1;
-        [SerializeField]
-        private bool active = false;
         [field: SerializeField] 
         private string enemyTag = "enemy";
+        [SerializeField]
+        private LayerMask wallLayerMask;
         private float elapsed = 0;
         private Damageable _target;
+        private Transform _targetTransform;
+
+        
+        private EnemyState state = EnemyState.Idling;
 
         private void Update()
         {
-            if (!active) return;
+            if (state is EnemyState.Seeking or EnemyState.Attacking) SeekForTarget();
+            if (state != EnemyState.Attacking) return;
             elapsed += Time.deltaTime;
             if (!(elapsed > TickDuration)) return;
             elapsed = 0;
             ApplyDamage();
+        }
+
+        private void SeekForTarget()
+        {
+            var direction = _targetTransform.position - transform.position;
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, direction, out hit, Mathf.Infinity, wallLayerMask))
+            {
+                Debug.Log("Seeking for target");
+                state = EnemyState.Seeking;
+                return;
+            }
+            Debug.Log("Attacking target");
+            state = EnemyState.Attacking;
         }
 
         public int GetDamage()
@@ -37,7 +52,7 @@ namespace PlayerHorde
 
         private void ApplyDamage()
         {
-            if (!active) return;
+            Debug.Log("Applying damage");
             _target?.Damage(GetDamage());
             elapsed = 0;
         }
@@ -46,6 +61,7 @@ namespace PlayerHorde
         {
             if (!other.CompareTag(enemyTag)) return;
             _target = other.gameObject.GetComponent<Damageable>();
+            _targetTransform = other.gameObject.GetComponent<Transform>();
             if (_target == null) return;
             StartAttack();
         }
@@ -58,13 +74,13 @@ namespace PlayerHorde
 
         private void StartAttack()
         {
-            active = true;
+            state = EnemyState.Attacking;
             elapsed = 0;
         }
 
         private void StopAttack()
         {
-            active = false;
+            state = EnemyState.Idling;
             elapsed = 0;
         }
     }
