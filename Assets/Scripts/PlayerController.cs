f4497270cd3937693f1e9a11c570385137665c49
Hankrecords <>
2023-02-05 10:59:52 +0100

    - fix player movement to make it smoother - fix some env objects angles allowing player to clip through

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,6 +11,8 @@ public class PlayerController : MonoBehaviour
     private float moveSpeed = 5f;
     private Vector2 moveInput = Vector2.zero;
     private float cameraAngle = 45;
+    private Vector3 smoothedMoveInput;
+    private Vector3 currentVelocity;
 
     private void Awake()
     {
@@ -21,7 +23,26 @@ public class PlayerController : MonoBehaviour
     {
         var velocity = new Vector3(moveInput.x, 0, moveInput.y) * (moveSpeed * Time.deltaTime);
         velocity = Quaternion.AngleAxis(cameraAngle, Vector3.up) * velocity;
-        controller.Move(velocity);
+
+        smoothedMoveInput = Vector3.SmoothDamp(
+            smoothedMoveInput,
+            velocity,
+            ref currentVelocity,
+            0.1f
+        );
+
+        RotateTowardsInput();
+
+        controller.Move(smoothedMoveInput);
+    }
+
+    private void RotateTowardsInput()
+    {
+        if (moveInput != Vector2.zero)
+        {
+            var targetAngle = Mathf.Atan2(smoothedMoveInput.x, smoothedMoveInput.z) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0, targetAngle, 0);
+        }
     }
 
     public void OnMove(InputValue value)
