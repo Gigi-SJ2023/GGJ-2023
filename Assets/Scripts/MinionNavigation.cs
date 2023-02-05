3dd730f7e05b118c0947dbc858e878eedee759f4
Hankrecords <>
2023-02-05 11:22:58 +0100

    - use raycast between player and playerwaypoint to decide which the horde minions should follow: if there's a wall between them, minions go towards the player object, otherwise they follow the waypoint

--- Assets/Scripts/MinionNavigation.cs
+++ Assets/Scripts/MinionNavigation.cs
@@ -6,18 +6,25 @@ using UnityEngine.AI;
 public class MinionNavigation : MonoBehaviour
 {
     private NavMeshAgent _navMeshAgent;
+    [field: SerializeField] public LayerMask wallLayerMask;
     [field: SerializeField] public Transform Followtransform { get; set; }
     [field: SerializeField] public float MinDistance { get; set; } = 5f;
+    [field: SerializeField] public string PlayerTag { get; set; } = "Player";
     [field: SerializeField] public string WaypointTag { get; set; } = "playerHordeWaypoint";
     [field: SerializeField] public string PlayerHordeAreaTag { get; set; } = "playerHordeSizeContainer";
     [field: SerializeField] public float BaseSpeed {get; set; } = 10f;
     [field: SerializeField] public float BaseAcceleration {get; set; } = 10f;
+    private Transform WaypointTransform;
+    private Transform PlayerTransform;
     private CapsuleCollider playerHorde;
 
     void Start()
     {
-        var playerGo = GameObject.FindGameObjectWithTag(WaypointTag);
-        Followtransform = playerGo.GetComponent<Transform>();
+        var waypointGo = GameObject.FindGameObjectWithTag(WaypointTag);
+        WaypointTransform = waypointGo.GetComponent<Transform>();
+        var playerGo = GameObject.FindGameObjectWithTag(PlayerTag);
+        PlayerTransform = playerGo.GetComponent<Transform>();
+        Followtransform = WaypointTransform;
         _navMeshAgent = GetComponent<NavMeshAgent>();
         var playerHordeArea = GameObject.FindGameObjectWithTag(PlayerHordeAreaTag);
         playerHorde = playerHordeArea.GetComponent<CapsuleCollider>();
@@ -28,10 +35,31 @@ public class MinionNavigation : MonoBehaviour
         return playerHorde.bounds.Contains(transform.position);
     }
 
+    private bool IsWaypointBlocked()
+    {
+        var direction = WaypointTransform.position - PlayerTransform.position;
+        RaycastHit hit;
+        return Physics.Raycast(
+            PlayerTransform.position,
+            direction,
+            out hit,
+            Vector3.Distance(WaypointTransform.position, PlayerTransform.position),
+            wallLayerMask
+        );
+    }
+
     // Update is called once per frame
     void Update()
     {
         if (Followtransform == null) return;
+
+        Followtransform = WaypointTransform;
+
+        if (IsWaypointBlocked())
+        {
+            Debug.Log("Waypoint blocked, using player");
+            Followtransform = PlayerTransform;
+        }
 
         var destination = Followtransform.position;
         
